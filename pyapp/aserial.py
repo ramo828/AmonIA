73b2772dffcfeba1f9dcf57059f89c2927d6733f
ramo828 <illegalism666@gmail.com>
2022-05-10 04:08:34 +0400

    add google csv and txt separate

--- pyapp/aserial.py
+++ pyapp/aserial.py
@@ -75,13 +75,65 @@ def dataSplit(data):
 
 if(csv_bool):
     csv_out = csv.writer(w, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    csv_out.writerow(["ID", "Kategoriya", "Prefix","Nömrə"])
+    csv_out.writerow(['Name', 'Given Name', 'Additional Name', 'Family Name', 'Yomi Name',
+       'Given Name Yomi', 'Additional Name Yomi', 'Family Name Yomi',
+       'Name Prefix', 'Name Suffix', 'Initials', 'Nickname', 'Short Name',
+       'Maiden Name', 'Birthday', 'Gender', 'Location', 'Billing Information',
+       'Directory Server', 'Mileage', 'Occupation', 'Hobby', 'Sensitivity',
+       'Priority', 'Subject', 'Notes', 'Language', 'Photo', 'Group Membership',
+       'Phone 1 - Type', 'Phone 1 - Value', 'Phone 2 - Type',
+       'Phone 2 - Value', 'Organization 1 - Type', 'Organization 1 - Name',
+       'Organization 1 - Yomi Name', 'Organization 1 - Title',
+       'Organization 1 - Department', 'Organization 1 - Symbol',
+       'Organization 1 - Location', 'Organization 1 - Job Description'])
 else:
     pass
 
-def writeCSV(_id,_cat,_pref,_data):
+def writeCSV(_name,_pref,_data):
 
-    csv_out.writerow([_id, _cat, _pref,_data])
+    csv_out.writerow([
+    _name, #0
+    _name, #1
+    '',    #2
+    '',    #3
+    '',    #4
+    '',    #5
+    '',    #6
+    '',    #7
+    '',    #8
+    '',    #9
+    '',    #10
+    '',    #11
+    '',    #12
+    '',    #13
+    '',    #14
+    '',    #15
+    '',    #16
+    '',    #17
+    '',    #18
+    '',    #19
+    '',    #20
+    '',    #21
+    '',    #22
+    '',    #23
+    '',    #24
+    '',    #25
+    '',    #26
+    '',    #27
+    '* myContacts',    #28
+    'Mobile',          #29
+    _pref+_data,       #30
+    '',    #31
+    '',    #32
+    '',    #33
+    '',    #34
+    '',    #35
+    '',    #36
+    '',    #37
+    '',    #38
+    '',    #39
+    '',    #40
+    ])
 
 def counter(data):
     ccl = []
@@ -117,9 +169,9 @@ for numb in tqdm(nData.splitlines()):          # Fayldaki melumatlari oxu
                     if(banner):
                         lib.setBanner(dataSplit(splData[2:]),lib.prefDigit(pref),categoryValue)
                     elif(csv_bool):
-                        catN = lib.getConvData(lib.prefDigit(pref),categoryValue,1)
+                        # catN = lib.getConvData(lib.prefDigit(pref),categoryValue,1)
                         prefN = lib.getConvData(lib.prefDigit(pref),categoryValue,0)
-                        writeCSV(htmlNumb,catN,prefN,dataSplit(splData[2:]))
+                        writeCSV("Metros "+str(htmlNumb),prefN,splData[2:])
                     else:
                         lib.setData(htmlNumb,dataSplit(splData[2:]),lib.prefDigit(pref),categoryValue)
 
@@ -138,9 +190,9 @@ for numb in tqdm(nData.splitlines()):          # Fayldaki melumatlari oxu
                     if(banner):
                         lib.setBanner(dataSplit(splData[2:]),lib.prefDigit(pref),categoryValue)
                     elif(csv_bool):
-                        catN = lib.getConvData(lib.prefDigit(pref),categoryValue,1)
+                        # catN = lib.getConvData(lib.prefDigit(pref),categoryValue,1)
                         prefN = lib.getConvData(lib.prefDigit(pref),categoryValue,0)
-                        writeCSV(htmlNumb,catN,prefN,dataSplit(splData[2:]))
+                        writeCSV("Metros "+str(htmlNumb),prefN,splData[2:])
                     else:
                         lib.setData(htmlNumb,dataSplit(splData[2:]),lib.prefDigit(pref),categoryValue)
 
@@ -150,7 +202,7 @@ print("\nÜmumi nömrə sayı: "+str(htmlNumb))
 print("\nFaylın adresi: "+fileName[fileNameIndex])
 
 if(text):
-    w.write(allNumbText)
+    w.write(dataSplit(allNumbText))
 else:
     if(banner):
         w.write(lib.toHTML1())
